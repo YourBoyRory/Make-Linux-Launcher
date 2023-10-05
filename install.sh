954d8ab039ccb82d82329fe659cb39084808136d
Rory <yourboyrory@gmail.com>
2023-10-05 12:42:29 -0400

    Change Script names

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 #!/bin/bash
 
-sudo cp ./makeShortcut /usr/local/bin/makeShortcut
+sudo cp ./makeShortcut.sh /usr/local/bin/makeShortcut
 sudo cp ./makeShortcutGUI.sh /usr/local/bin/makeShortcutGUI
 sudo chmod 755 /usr/local/bin/makeShortcut
 sudo chmod 755 /usr/local/bin/makeShortcutGUI
