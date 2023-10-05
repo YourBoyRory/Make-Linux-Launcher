f13dabbae72354c301f29bf592584089c1403b33
Rory <yourboyrory@gmail.com>
2023-10-05 12:41:11 -0400

    Added installer and uninstaller

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#!/bin/bash
+
+sudo cp ./makeShortcut /usr/local/bin/makeShortcut
+sudo cp ./makeShortcutGUI.sh /usr/local/bin/makeShortcutGUI
+sudo chmod 755 /usr/local/bin/makeShortcut
+sudo chmod 755 /usr/local/bin/makeShortcutGUI
